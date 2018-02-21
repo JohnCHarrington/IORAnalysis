2902330f983e296bc84c7e1c0bd3e7176700ba1f
JOHNCH\John <john@johnch.com>
2018-02-21 15:25:15 +0000

    Forgot setup.py!

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='IORAnalysis',
+      version='0.1',
+      description='Utilities for the analysis of IOR yachts',
+      author='John Harrington',
+      author_email='john@johnch.com',
+      packages=['IORAnalysis']
+     )
